62ed2c4c669d5c550c5d53577a6468bd2108eb9c
dahakagh <Marshev123@yandex.ru>
2021-06-24 16:05:08 +0300

    pipes, validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsEmail, Length } from "class-validator";
 
 export class CreateUserDto {
   @ApiProperty({ example: "email@email.com", description: "Почтовый адрес" })
+  @IsString({ message: "Должно быть строкой" })
+  @IsEmail({}, { message: "Некорректный email" })
   readonly email: string;
   @ApiProperty({ example: "qwerty", description: "Пароль" })
+  @IsString({ message: "Должно быть строкой" })
+  @Length(4, 16, { message: "Неверная длина пароля" })
   readonly password: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,13 @@
-import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
-import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 import { Roles } from "src/auth/roles-auth.decorator";
 import { RolesGuard } from "src/auth/roles.guard";
 import { AddRoleDto } from "./dto/add-role.dto";
@@ -16,6 +23,7 @@ export class UsersController {
 
   @ApiOperation({ summary: "Создание пользователя" })
   @ApiResponse({ status: 200, type: User })
+  @UsePipes(ValidationPipe)
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.usersService.createUser(userDto);
